0ce1622d45c2201865fa21a69d8929653f8f1ab5
Tomoyuki Aota <tomoyukia@gmail.com>
2022-01-13 00:30:00 +0900

    Use MUI components to update the header style.

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,11 +1,23 @@
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+
 import classes from './Header.module.scss';
 
 const Header: React.FC<{ className?: string }> = (props) => {
   return (
     <div className={`${props.className} ${classes.header}`}>
       <div>Select your photo to display the EXIF data:</div>
-      <input></input>
-      <button>Select</button>
+      <div className={classes['textfield-and-button']}>
+        <TextField
+          fullWidth
+          InputProps={{
+            readOnly: true,
+          }}
+          size='small'
+          variant='outlined'
+        />
+        <Button variant='outlined'>Select</Button>
+      </div>
     </div>
   );
 };
