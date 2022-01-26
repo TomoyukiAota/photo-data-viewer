fd0aaf4de962e5705a19ce5707edf81de6f4700f
Tomoyuki Aota <tomoyukia@gmail.com>
2022-01-26 18:55:40 +0900

    Refactor createGeneralDataRows.

--- components/photo-data/general-data-row.tsx
+++ components/photo-data/general-data-row.tsx
@@ -2,43 +2,35 @@ import LatLngRow from './LatLngRow';
 import { LoadedPhotoData } from '../../store/loaded-photo-data';
 import { GridRow } from './grid-row';
 
-interface GeneralDataRow extends GridRow {
+interface NameAndValue {
   name: string;
   value: string | JSX.Element;
 }
 
+export interface GeneralDataRow extends GridRow, NameAndValue {}
+
 export function createGeneralDataRows(
   loadedPhotoData: LoadedPhotoData | null
 ): GeneralDataRow[] {
-  return [
+  const nameAndValues: NameAndValue[] = [
     {
-      id: 0,
-      parentId: null,
       name: 'File Name',
       value: loadedPhotoData?.file?.name ?? '',
     },
     {
-      id: 1,
-      parentId: null,
       name: 'File Size',
       value: loadedPhotoData?.file?.size?.displayString ?? '',
     },
     {
-      id: 2,
-      parentId: null,
       name: 'Last Modified',
       value: loadedPhotoData?.file?.lastModified?.localizedFormat ?? '',
     },
     {
-      id: 3,
-      parentId: null,
       name: 'Date Taken',
       value: loadedPhotoData?.exif?.dateTimeOriginal?.localizedFormat ?? '',
     },
     // TODO: Add Image Dimensions
     {
-      id: 4,
-      parentId: null,
       name: 'Latitude, Longitude',
       value: (
         <LatLngRow
@@ -48,4 +40,14 @@ export function createGeneralDataRows(
       ),
     },
   ];
+
+  const generalDataRows = nameAndValues.map((nameAndValue, index) => {
+    return {
+      id: index,
+      parentId: null,
+      ...nameAndValue,
+    };
+  });
+
+  return generalDataRows;
 }
