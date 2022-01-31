4cc8da70e31a1ba72b4a024d5b7a44f338579d84
Tomoyuki Aota <tomoyukia@gmail.com>
2022-02-01 00:27:02 +0900

    Display location-data-not-available message on the map.

--- components/PhotoMap.tsx
+++ components/PhotoMap.tsx
@@ -1,8 +1,29 @@
+import { useContext } from 'react';
+import PhotoContext from '../store/photo-context';
 import LeafletLoader from './leaflet/LeafletLoader';
 import classes from './PhotoMap.module.scss';
 
+const LatLngNotAvailableOverlay: React.FC<{ className?: string }> = (props) => {
+  return (
+    <div className={classes.overlay}>
+      <div className={classes['overlay-content']}>
+        The location data (latitude and longitude) is not available.
+      </div>
+    </div>
+  );
+};
+
 const PhotoMap: React.FC<{ className?: string }> = (props) => {
-  return <LeafletLoader />;
+  const photoCtx = useContext(PhotoContext);
+  const isFileLoaded = !!photoCtx.loadedPhotoData?.isFileLoaded;
+  const isLatLngAvailable = !!photoCtx.loadedPhotoData?.isLatLngAvailable;
+
+  return (
+    <div className={classes.container}>
+      {isFileLoaded && !isLatLngAvailable && <LatLngNotAvailableOverlay />}
+      <LeafletLoader />
+    </div>
+  );
 };
 
 export default PhotoMap;
