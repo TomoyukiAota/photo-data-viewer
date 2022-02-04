0f19f25b446fef8d5bc25ef5d6b07b76ecb887cb
Tomoyuki Aota <tomoyukia@gmail.com>
2022-02-05 01:57:30 +0900

    Support displaying HEIF file.

--- store/photo/PhotoProvider.tsx
+++ store/photo/PhotoProvider.tsx
@@ -1,14 +1,43 @@
+import classNames from 'classnames';
 import Image from 'next/image';
 import { useContext, useState } from 'react';
 
 import overrideNextImage from '../../styles/override-next-image.module.scss';
+import { isHeif } from '../../utils/filename-extension';
 import DialogContext from '../dialog/dialog-context';
 import { createLoadedPhotoData, LoadedPhotoData } from './loaded-photo-data';
 import PhotoContext from './photo-context';
 import classes from './PhotoProvider.module.scss';
 
-const PhotoProviderText: React.FC = (props) => {
-  return <div className={classes.text}>{props.children}</div>;
+async function getBlobForHeif(file: File) {
+  let blob: Blob | null = null;
+  try {
+    const heic2any = require('heic2any'); // requre here to make debugging easier
+    blob = await heic2any({ blob: file });
+  } catch (error) {
+    console.log(
+      `An error is thrown when handling the HEIF file ${file.name}. error: `,
+      error
+    );
+  }
+  return blob;
+}
+
+async function getImageUrl(file: File) {
+  let fileOrBlob: File | Blob = file;
+  if (isHeif(file.name)) {
+    fileOrBlob = (await getBlobForHeif(file)) ?? file;
+  }
+  const url = URL.createObjectURL(fileOrBlob);
+  return url;
+}
+
+const PhotoProviderText: React.FC<{ className?: string }> = (props) => {
+  return (
+    <div className={classNames(classes.text, props.className)}>
+      {props.children}
+    </div>
+  );
 };
 
 const PhotoProvider: React.FC = (props) => {
@@ -30,24 +59,34 @@ const PhotoProvider: React.FC = (props) => {
   const loadPhoto = async (file: File) => {
     console.log('loadPhoto called. file: ', file);
 
+    // Show loading message because it takes time to load HEIF file.
+    if (isHeif(file.name)) {
+      setLoadedPhotoImage(() => (
+        <PhotoProviderText className={classes['loading-text']}>
+          Loading the selected photo...
+        </PhotoProviderText>
+      ));
+    }
+
+    const data = await createLoadedPhotoData(file);
+    console.log('LoadedPhotoData', data);
+    setLoadedPhotoData(data);
+    if (!data?.isExifAvailable) {
+      dialogCtx.unsupportedFileSelected.open();
+    }
+
+    const url = await getImageUrl(file);
+
     setLoadedPhotoImage(() => (
       <div className={overrideNextImage.imageContainer}>
         <Image
           className={overrideNextImage.image}
-          src={URL.createObjectURL(file)}
+          src={url}
           alt={file.name}
           layout='fill'
         ></Image>
       </div>
     ));
-
-    const data = await createLoadedPhotoData(file);
-    console.log('LoadedPhotoData', data);
-    setLoadedPhotoData(data);
-
-    if (!data?.isExifAvailable) {
-      dialogCtx.unsupportedFileSelected.open();
-    }
   };
 
   return (
---
+++ utils/filename-extension.ts
@@ -0,0 +1,25 @@
+export class FilenameExtension {
+  private static readonly jpegExtensions: ReadonlyArray<string> = ['jpeg', 'jpg', 'jpe', 'jfif', 'jfi', 'jif'];
+  private static readonly tiffExtensions: ReadonlyArray<string> = ['tiff', 'tif'];
+  private static readonly pngExtensions: ReadonlyArray<string> = ['png'];
+  private static readonly heifExtensions: ReadonlyArray<string> = ['heif', 'heic'];
+  private static readonly webpExtensions: ReadonlyArray<string> = ['webp'];
+  private static readonly movExtensions: ReadonlyArray<string> = ['mov'];
+
+  public static isJpeg(extension: string) { return this.jpegExtensions.includes(extension.toLowerCase()); }
+  public static isTiff(extension: string) { return this.tiffExtensions.includes(extension.toLowerCase()); }
+  public static isPng(extension: string) { return this.pngExtensions.includes(extension.toLowerCase()); }
+  public static isHeif(extension: string) { return this.heifExtensions.includes(extension.toLowerCase()); }
+  public static isWebp(extension: string) { return this.webpExtensions.includes(extension.toLowerCase()); }
+  public static isMov(extension: string) { return this.movExtensions.includes(extension.toLowerCase()); }
+}
+
+export function getExtension(filename: string): string {
+  const extension = filename?.split?.('.')?.pop?.();
+  return extension ?? '';
+}
+
+export function isHeif(filename: string) {
+  const extension = getExtension(filename);
+  return FilenameExtension.isHeif(extension);
+}
