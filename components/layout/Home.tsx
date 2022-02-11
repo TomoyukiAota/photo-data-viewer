09479207f066b9285f9a17cd57f88726c77c03b5
Tomoyuki Aota <tomoyukia@gmail.com>
2022-02-11 21:49:01 +0900

    Implement responsive layout.

--- components/layout/Home.tsx
+++ components/layout/Home.tsx
@@ -1,19 +1,41 @@
+import { useWindowWidth } from '@react-hook/window-size/throttled';
+
 import PhotoData from '../photo-data/PhotoData';
 import PhotoImageContainer from '../photo-image/PhotoImageContainer';
 import PhotoMap from '../PhotoMap';
 
 import Header from './Header';
+import HomeNarrowLayout from './HomeNarrowLayout';
 import HomeWideLayout from './HomeWideLayout';
 import classes from './Home.module.scss';
 
 const Home: React.FC = () => {
+  const windowWidth = useWindowWidth();
+  const shouldUseWideLayout = windowWidth > 1200;
+
+  const header = <Header />;
+  const photoImage = <PhotoImageContainer />;
+  const photoMap = <PhotoMap />;
+  const photoData = <PhotoData />;
+
   return (
-    <HomeWideLayout
-      header={<Header />}
-      photoImage={<PhotoImageContainer />}
-      photoMap={<PhotoMap />}
-      photoData={<PhotoData />}
-    />
+    <>
+      {shouldUseWideLayout ? (
+        <HomeWideLayout
+          header={header}
+          photoImage={photoImage}
+          photoMap={photoMap}
+          photoData={photoData}
+        />
+      ) : (
+        <HomeNarrowLayout
+          header={header}
+          photoImage={photoImage}
+          photoMap={photoMap}
+          photoData={photoData}
+        />
+      )}
+    </>
   );
 };
 
---
+++ components/layout/HomeNarrowLayout.tsx
@@ -0,0 +1,51 @@
+import { ReflexContainer, ReflexElement, ReflexSplitter } from 'react-reflex';
+import 'react-reflex/styles.css';
+
+import classes from './HomeNarrowLayout.module.scss';
+
+const HomeNarrowLayout: React.FC<{
+  header: JSX.Element;
+  photoImage: JSX.Element;
+  photoMap: JSX.Element;
+  photoData: JSX.Element;
+}> = (props) => {
+  const headerHeight = 100;
+  const isHeaderVisible = true;
+
+  return (
+    <div className={classes.home}>
+      <ReflexContainer orientation='horizontal'>
+        {isHeaderVisible && (
+          <ReflexElement
+            minSize={headerHeight}
+            maxSize={headerHeight}
+            style={{
+              height: `${headerHeight}px`,
+              minHeight: `${headerHeight}px`,
+              maxHeight: `${headerHeight}px`,
+            }}
+          >
+            {props.header}
+          </ReflexElement>
+        )}
+        <ReflexElement>
+          <ReflexContainer orientation='horizontal'>
+            <ReflexElement minSize={100}>{props.photoImage}</ReflexElement>
+            <ReflexSplitter
+              className={classes['horizontal-splitter']}
+              style={{ height: '8px', border: 0 }}
+            />
+            <ReflexElement minSize={100}>{props.photoMap}</ReflexElement>
+            <ReflexSplitter
+              className={classes['horizontal-splitter']}
+              style={{ height: '8px', border: 0 }}
+            />
+            <ReflexElement minSize={100}>{props.photoData}</ReflexElement>
+          </ReflexContainer>
+        </ReflexElement>
+      </ReflexContainer>
+    </div>
+  );
+};
+
+export default HomeNarrowLayout;
