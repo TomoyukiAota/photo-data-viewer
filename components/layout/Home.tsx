18b33ca690bbb1755c968aa804bd32638701908a
Tomoyuki Aota <tomoyukia@gmail.com>
2022-02-11 21:26:45 +0900

    Extract HomeWideLayout.

--- components/layout/Home.tsx
+++ components/layout/Home.tsx
@@ -1,60 +1,19 @@
-import { ReflexContainer, ReflexElement, ReflexSplitter } from 'react-reflex';
-import 'react-reflex/styles.css';
-
 import PhotoData from '../photo-data/PhotoData';
 import PhotoImageContainer from '../photo-image/PhotoImageContainer';
 import PhotoMap from '../PhotoMap';
 
 import Header from './Header';
+import HomeWideLayout from './HomeWideLayout';
 import classes from './Home.module.scss';
 
 const Home: React.FC = () => {
-  const headerHeight = 100;
-  const isHeaderVisible = true;
-
   return (
-    <div className={classes.home}>
-      <ReflexContainer orientation='horizontal'>
-        {isHeaderVisible && (
-          <ReflexElement
-            minSize={headerHeight}
-            maxSize={headerHeight}
-            style={{
-              height: `${headerHeight}px`,
-              minHeight: `${headerHeight}px`,
-              maxHeight: `${headerHeight}px`,
-            }}
-          >
-            <Header></Header>
-          </ReflexElement>
-        )}
-        <ReflexElement>
-          <ReflexContainer orientation='vertical'>
-            <ReflexElement minSize={100}>
-              <ReflexContainer orientation='horizontal'>
-                <ReflexElement minSize={100}>
-                  <PhotoImageContainer />
-                </ReflexElement>
-                <ReflexSplitter
-                  className={classes['horizontal-splitter']}
-                  style={{ height: '8px', border: 0 }}
-                />
-                <ReflexElement minSize={100}>
-                  <PhotoMap />
-                </ReflexElement>
-              </ReflexContainer>
-            </ReflexElement>
-            <ReflexSplitter
-              className={classes['vertical-splitter']}
-              style={{ width: '8px', border: 0 }}
-            />
-            <ReflexElement minSize={100}>
-              <PhotoData />
-            </ReflexElement>
-          </ReflexContainer>
-        </ReflexElement>
-      </ReflexContainer>
-    </div>
+    <HomeWideLayout
+      header={<Header />}
+      photoImage={<PhotoImageContainer />}
+      photoMap={<PhotoMap />}
+      photoData={<PhotoData />}
+    />
   );
 };
 
---
+++ components/layout/HomeWideLayout.tsx
@@ -0,0 +1,55 @@
+import { ReflexContainer, ReflexElement, ReflexSplitter } from 'react-reflex';
+import 'react-reflex/styles.css';
+
+import classes from './HomeWideLayout.module.scss';
+
+const HomeWideLayout: React.FC<{
+  header: JSX.Element;
+  photoImage: JSX.Element;
+  photoMap: JSX.Element;
+  photoData: JSX.Element;
+}> = (props) => {
+  const headerHeight = 100;
+  const isHeaderVisible = true;
+
+  return (
+    <div className={classes.home}>
+      <ReflexContainer orientation='horizontal'>
+        {isHeaderVisible && (
+          <ReflexElement
+            minSize={headerHeight}
+            maxSize={headerHeight}
+            style={{
+              height: `${headerHeight}px`,
+              minHeight: `${headerHeight}px`,
+              maxHeight: `${headerHeight}px`,
+            }}
+          >
+            {props.header}
+          </ReflexElement>
+        )}
+        <ReflexElement>
+          <ReflexContainer orientation='vertical'>
+            <ReflexElement minSize={100}>
+              <ReflexContainer orientation='horizontal'>
+                <ReflexElement minSize={100}>{props.photoImage}</ReflexElement>
+                <ReflexSplitter
+                  className={classes['horizontal-splitter']}
+                  style={{ height: '8px', border: 0 }}
+                />
+                <ReflexElement minSize={100}>{props.photoMap}</ReflexElement>
+              </ReflexContainer>
+            </ReflexElement>
+            <ReflexSplitter
+              className={classes['vertical-splitter']}
+              style={{ width: '8px', border: 0 }}
+            />
+            <ReflexElement minSize={100}>{props.photoData}</ReflexElement>
+          </ReflexContainer>
+        </ReflexElement>
+      </ReflexContainer>
+    </div>
+  );
+};
+
+export default HomeWideLayout;
