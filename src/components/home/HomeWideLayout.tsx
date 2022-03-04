a81f902f0f93d7bec106e3ac7835b784aee019a2
Tomoyuki Aota <tomoyukia@gmail.com>
2022-03-05 03:42:02 +0900

    Handle the weird issue that the scrollbar appears when image/map panes are evenly split (i.e. default)

--- src/components/home/HomeWideLayout.tsx
+++ src/components/home/HomeWideLayout.tsx
@@ -54,7 +54,10 @@ const HomeWideLayout: React.FC<{
         )}
         <ReflexElement>
           <ReflexContainer orientation='vertical'>
-            <ReflexElement minSize={100}>
+            <ReflexElement
+              minSize={100}
+              className={classes['image-and-map-container']}
+            >
               <ReflexContainer orientation='horizontal'>
                 <ReflexElement
                   minSize={100}
