3963a9bfad64df7dfb31539d9655d53ff68c6a1c
Tomoyuki Aota <tomoyukia@gmail.com>
2022-03-29 05:17:26 +0900

    Use the default value constant for the window title.

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,12 +1,13 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import About from '../components/about/About';
+import { documentTitleDefaultValue } from '../context/document-title/document-title-default-value';
 
 const AboutPage: NextPage = () => {
   return (
     <>
       <Head>
-        <title>About - Photo Data Viewer</title>
+        <title>{`About - ${documentTitleDefaultValue}`}</title>
         <meta name='description' content='View your photo data.' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,12 +1,13 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import UserSettings from '../components/user-settings/UserSettings';
+import { documentTitleDefaultValue } from '../context/document-title/document-title-default-value';
 
 const SettingsPage: NextPage = () => {
   return (
     <>
       <Head>
-        <title>Settings - Photo Data Viewer</title>
+        <title>{`Settings - ${documentTitleDefaultValue}`}</title>
         <meta name='description' content='View your photo data.' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
