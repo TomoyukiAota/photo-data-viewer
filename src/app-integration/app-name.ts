d8fbc877f2ead8c846ec1af19d2730279ce3698e
Tomoyuki Aota <tomoyukia@gmail.com>
2022-03-29 21:40:25 +0900

    Fix build error.

--- src/app-integration/app-name.ts
+++ src/app-integration/app-name.ts
@@ -3,7 +3,7 @@ import { AppIntegration } from './app-integration';
 export const appName = {
   standalone: 'Photo Data Viewer',
   plm: 'Photo Location Map',
-} as const;
+};
 
 export function getAppName() {
   return AppIntegration.isPlm ? appName.plm : appName.standalone;
