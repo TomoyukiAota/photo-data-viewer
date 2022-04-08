68946a47289e2a5f523699ead531ae4fbbdac52d
Tomoyuki Aota <tomoyukia@gmail.com>
2022-04-09 02:26:14 +0900

    Organize functions related to loadPhoto.

--- src/context/photo/photo-context.ts
+++ src/context/photo/photo-context.ts
@@ -5,14 +5,14 @@ import { LoadedPhotoData } from './loaded-photo-data';
 type PhotoContextType = {
   loadedPhotoData: LoadedPhotoData | null,
   loadedPhotoImage: JSX.Element,
-  loadPhoto: (file: File, option?: { isReload: boolean }) => void,
+  loadPhoto: (file: File) => void,
   reloadPhoto: () => void,
 };
 
 const PhotoContext = React.createContext<PhotoContextType>({
   loadedPhotoData: null,
   loadedPhotoImage: React.createElement('div'),
-  loadPhoto: (file: File, option?: { isReload: boolean }) => { },
+  loadPhoto: (file: File) => { },
   reloadPhoto: () => { },
 });
 
