9cfa2acf8e6908b0115852261aff6a1b63e53bd2
Tomoyuki Aota <tomoyukia@gmail.com>
2023-08-27 01:17:07 +0900

    Load IMG_5769.JGP fetched from photo-data-viewer-resources repo.

--- src/components/demo/Demo.tsx
+++ src/components/demo/Demo.tsx
@@ -1,13 +1,34 @@
 import { Button } from "@mui/material";
+import { useContext } from 'react';
 
+import PhotoContext from '../../context/photo/photo-context';
 import classes from './Demo.module.scss';
 
+function blobToFile(blob: Blob, fileName:string): File {
+  return new File(
+    [blob],
+    fileName,
+    {
+      lastModified: new Date().getTime(), // lastModified is the current time, which is the same as the file downloaded to the disk using web browsers.
+      type: blob.type
+    }
+  )
+}
+
 const Demo: React.FC = () => {
+  const photoCtx = useContext(PhotoContext);
+
   const handleButtonClicked: React.MouseEventHandler<
       HTMLButtonElement
-  > = () => {
-    console.log('Run Demo button is clicked.');
-    // Select the photo, and switch to the home page.
+  > = async () => {
+    // Using jsDelivr for 'https://github.com/TomoyukiAota/photo-data-viewer-resources/blob/d803c13d8718d4ac8b3dad8dfb9597d441040f4d/photos/IMG_5769.JPG?raw=true';
+    // This is to circumvent the CORS error which occurs when the file in GitHub is directly fetched.
+    const photoUrl = 'https://cdn.jsdelivr.net/gh/TomoyukiAota/photo-data-viewer-resources@d803c13d8718d4ac8b3dad8dfb9597d441040f4d/photos/IMG_5769.JPG'
+
+    const response = await fetch(photoUrl);
+    const blob = await response.blob();
+    const file = blobToFile(blob, 'IMG_5769.JGP');
+    photoCtx.loadPhoto(file);
   };
 
   return (
