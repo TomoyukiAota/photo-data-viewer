b76aa5e48962f21b1d002b50889c3306d03c7e04
Tomoyuki Aota <tomoyukia@gmail.com>
2023-08-26 18:40:15 +0900

    Add "Run Demo" button.

--- src/components/demo/Demo.tsx
+++ src/components/demo/Demo.tsx
@@ -1,6 +1,15 @@
+import { Button } from "@mui/material";
+
 import classes from './Demo.module.scss';
 
 const Demo: React.FC = () => {
+  const handleButtonClicked: React.MouseEventHandler<
+      HTMLButtonElement
+  > = () => {
+    console.log('Run Demo button is clicked.');
+    // Select the photo, and switch to the home page.
+  };
+
   return (
     <div className={classes.container}>
       <div className={classes['page-title']}>Demo</div>
@@ -8,6 +17,16 @@ const Demo: React.FC = () => {
         <div>
           Once &quot;Run Demo&quot; button is clicked, this application behaves as if this photo is selected.
         </div>
+      </div>
+      <div className={classes['run-demo']}>
+        <Button
+          className={classes['run-demo-button']}
+          variant='outlined'
+          onClick={handleButtonClicked}
+          sx={{ textTransform: 'none' }}
+        >
+          Run Demo
+        </Button>
       </div>
     </div>
   );
