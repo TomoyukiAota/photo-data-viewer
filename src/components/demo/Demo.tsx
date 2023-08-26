b31c695c562b525d383bce24e098f2fd0b3f0875
Tomoyuki Aota <tomoyukia@gmail.com>
2023-08-27 01:17:56 +0900

    Use single quote.

--- src/components/demo/Demo.tsx
+++ src/components/demo/Demo.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@mui/material";
+import { Button } from '@mui/material';
 import { useContext } from 'react';
 
 import PhotoContext from '../../context/photo/photo-context';
