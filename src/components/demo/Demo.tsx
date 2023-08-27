556ad72127161603e763748001579ae3a5753309
Tomoyuki Aota <tomoyukia@gmail.com>
2023-08-27 17:57:10 +0900

    Format code.

--- src/components/demo/Demo.tsx
+++ src/components/demo/Demo.tsx
@@ -4,19 +4,20 @@ import { useContext } from 'react';
 
 import PhotoContext from '../../context/photo/photo-context';
 import { sleep } from '../../utils/sleep';
-import { blobToFile } from "./blob-to-file";
+import { blobToFile } from './blob-to-file';
 import classes from './Demo.module.scss';
 
 // Using jsDelivr for 'https://github.com/TomoyukiAota/photo-data-viewer-resources/blob/bdd784c66d9b891277f42d1f2b7f4ec65b8c8e5d/photos/Demo%20(Apple%20Park%20Visitor%20Center).JPG?raw=true';
 // This is to circumvent the CORS error which occurs when the file in GitHub is directly fetched.
-const photoUrlForDemo = 'https://cdn.jsdelivr.net/gh/TomoyukiAota/photo-data-viewer-resources@bdd784c66d9b891277f42d1f2b7f4ec65b8c8e5d/photos/Demo%20(Apple%20Park%20Visitor%20Center).JPG'
+const photoUrlForDemo =
+  'https://cdn.jsdelivr.net/gh/TomoyukiAota/photo-data-viewer-resources@bdd784c66d9b891277f42d1f2b7f4ec65b8c8e5d/photos/Demo%20(Apple%20Park%20Visitor%20Center).JPG';
 
 const Demo: React.FC = () => {
   const photoCtx = useContext(PhotoContext);
   const router = useRouter();
 
   const handleButtonClicked: React.MouseEventHandler<
-      HTMLButtonElement
+    HTMLButtonElement
   > = async () => {
     await router.push('/');
     await sleep(1000); // After navigating to Home page, wait for some time. This makes the demo look better.
@@ -31,12 +32,9 @@ const Demo: React.FC = () => {
       <div className={classes['page-title']}>Demo</div>
       <div className={classes['messages']}>
         <div>
-          Once &quot;Run Demo&quot; button is clicked, this application behaves as if{' '}
-          <a
-            href={photoUrlForDemo}
-            target='_blank'
-            rel='noopener'
-          >
+          Once &quot;Run Demo&quot; button is clicked, this application behaves
+          as if{' '}
+          <a href={photoUrlForDemo} target='_blank' rel='noopener'>
             this photo
           </a>{' '}
           is selected.
