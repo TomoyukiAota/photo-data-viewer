243d7ebfe1c17d5e5d197a9adde08794b642838c
Tomoyuki Aota <tomoyukia@gmail.com>
2023-08-28 20:49:04 +0900

    Change the file name for the demo feature.

--- src/components/demo/Demo.tsx
+++ src/components/demo/Demo.tsx
@@ -7,10 +7,10 @@ import { sleep } from '../../utils/sleep';
 import { blobToFile } from './blob-to-file';
 import classes from './Demo.module.scss';
 
-// Using jsDelivr for 'https://github.com/TomoyukiAota/photo-data-viewer-resources/blob/bdd784c66d9b891277f42d1f2b7f4ec65b8c8e5d/photos/Demo%20(Apple%20Park%20Visitor%20Center).JPG?raw=true';
+// Using jsDelivr for 'https://github.com/TomoyukiAota/photo-data-viewer-resources/blob/2ba9ef57fd580cdabc4b26102098064e0b3c8505/photos/Demo%20(Apple%20Park).jpg?raw=true';
 // This is to circumvent the CORS error which occurs when the file in GitHub is directly fetched.
 const photoUrlForDemo =
-  'https://cdn.jsdelivr.net/gh/TomoyukiAota/photo-data-viewer-resources@bdd784c66d9b891277f42d1f2b7f4ec65b8c8e5d/photos/Demo%20(Apple%20Park%20Visitor%20Center).JPG';
+  'https://cdn.jsdelivr.net/gh/TomoyukiAota/photo-data-viewer-resources@2ba9ef57fd580cdabc4b26102098064e0b3c8505/photos/Demo%20(Apple%20Park).jpg';
 
 const Demo: React.FC = () => {
   const photoCtx = useContext(PhotoContext);
@@ -23,7 +23,7 @@ const Demo: React.FC = () => {
     await sleep(1000); // After navigating to Home page, wait for some time. This makes the demo look better.
     const response = await fetch(photoUrlForDemo);
     const blob = await response.blob();
-    const file = blobToFile(blob, 'Demo (Apple Park Visitor Center).JPG');
+    const file = blobToFile(blob, 'Demo (Apple Park).jpg');
     photoCtx.loadPhoto(file);
   };
 
